c3396b8f72aa1a4a4083e8c8e41b37fb7a697692
Abthakur-hub <abhishekthakur48700@gmail.com>
2026-07-19 09:45:50 +0530

    Solved CF A. Perfect Permutation

---
+++ APerfectPermutation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class APerfectPermutation{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if(n % 2 == 1){
+            System.out.println(-1);
+            return;
+        }
+
+        for(int i=1; i<=n; i+=2){
+            System.out.print((i + 1) + " " + i + " ");
+        }
+    }
+}
