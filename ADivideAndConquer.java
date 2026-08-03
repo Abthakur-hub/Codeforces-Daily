02a1bb117ea1b2fd8dc97437b84e3298cde62ec8
Abthakur-hub <abhishekthakur48700@gmail.com>
2026-08-03 10:23:03 +0530

    Solved

---
+++ ADivideAndConquer.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ADivideAndConquer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n-->0){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if(x%y==0) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
