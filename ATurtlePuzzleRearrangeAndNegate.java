23d54394daa56a3af2eb77489e71be9aa41a4825
Abthakur-hub <abhishekthakur48700@gmail.com>
2026-08-18 20:25:30 +0530

    Solved

---
+++ ATurtlePuzzleRearrangeAndNegate.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ATurtlePuzzleRearrangeAndNegate{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int sum = 0;
+            for(int i=0; i<n; i++){
+                int x = sc.nextInt();
+                sum += Math.abs(x);
+            }
+            System.out.println(sum);
+        }
+    }
+}
