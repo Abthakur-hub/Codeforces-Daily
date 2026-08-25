aff5c90c744a1313f8a910a614cfd57fa90d040f
Abthakur-hub <abhishekthakur48700@gmail.com>
2026-08-25 11:04:28 +0530

    Solved

---
+++ ASakurakoAndKosuke.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ASakurakoAndKosuke {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        while(n-->0){
+            int a = sc.nextInt();
+            if(a%2==0) System.out.println("Sakurako");
+            else System.out.println("Kosuke");
+        }
+    }
+}
