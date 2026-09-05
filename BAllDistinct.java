2dab731545b4ce1515d777f75e8c49fe7eb7d2ef
Abthakur-hub <abhishekthakur48700@gmail.com>
2026-09-05 23:58:37 +0530

    Solved

---
+++ BAllDistinct.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class BAllDistinct{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+            HashSet<Integer> set = new HashSet<>();
+            for(int i=0; i<n; i++){
+                set.add(sc.nextInt());
+            }
+            int d = set.size();
+            if((n - d) % 2 == 0){
+                System.out.println(d);
+            }else{
+                System.out.println(d - 1);
+            }
+        }
+    }
+}
